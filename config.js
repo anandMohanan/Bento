b60c58f39170bcb6e80b8a94b9cfd0a39da43280
anandMohanan <ananthkvmohanan@gmail.com>
2021-09-16 10:24:39 +0530

    fourth commit

--- config.js
+++ config.js
@@ -36,45 +36,45 @@ const CONFIG = {
   // │  ├─┤├┬┘ ││└─┐
   // └─┘┴ ┴┴└──┴┘└─┘
 
-  // Links
-  // cards: [
-  //   {
-  //     id: "1",
-  //     name: "Github",
-  //     icon: "github",
-  //     link: "https://github.com/",
-  //   },
-  //   // {
-  //   //   id: "2",
-  //   //   name: "Mail",
-  //   //   icon: "mail",
-  //   //   link: "https://mail.protonmail.com/",
-  //   // },
-  //   // {
-  //   //   id: "3",
-  //   //   name: "Todoist",
-  //   //   icon: "trello",
-  //   //   link: "https://calendar.google.com/calendar/r",
-  //   // },
-  //   // {
-  //   //   id: "4",
-  //   //   name: "Calendar",
-  //   //   icon: "calendar",
-  //   //   link: "https://calendar.google.com/calendar/r",
-  //   // },
-  //   {
-  //     id: "5",
-  //     name: "Reddit",
-  //     icon: "bookmark",
-  //     link: "https://reddit.com",
-  //   },
-  // {
-  //   id: "6",
-  //   name: "Odysee",
-  //   icon: "youtube",
-  //   link: "https://odysee.com/",
-  // },
-  //],
+  //Links
+  cards: [
+    {
+      id: "1",
+      name: "Github",
+      icon: "github",
+      link: "https://github.com/",
+    },
+    // {
+    //   id: "2",
+    //   name: "Mail",
+    //   icon: "mail",
+    //   link: "https://mail.protonmail.com/",
+    // },
+    // {
+    //   id: "3",
+    //   name: "Todoist",
+    //   icon: "trello",
+    //   link: "https://calendar.google.com/calendar/r",
+    // },
+    // {
+    //   id: "4",
+    //   name: "Calendar",
+    //   icon: "calendar",
+    //   link: "https://calendar.google.com/calendar/r",
+    // },
+    {
+      id: "5",
+      name: "Reddit",
+      icon: "bookmark",
+      link: "https://reddit.com",
+    },
+    // {
+    //   id: "6",
+    //   name: "Odysee",
+    //   icon: "youtube",
+    //   link: "https://odysee.com/",
+    // },
+  ],
 
   // ┬  ┬┌─┐┌┬┐┌─┐
   // │  │└─┐ │ └─┐
