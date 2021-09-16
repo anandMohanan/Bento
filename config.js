90fd59035acb1f67f7b2df1c697269a1cd3a7478
anandMohanan <ananthkvmohanan@gmail.com>
2021-09-16 10:15:30 +0530

    sdfdsfdsf

--- config.js
+++ config.js
@@ -11,26 +11,26 @@ const CONFIG = {
   // └─┘┴ ┴└─┘┴└─┘└─┘
 
   // General
-  name: 'John',
+  name: "Anand",
   imageBackground: false,
   openInNewTab: true,
   twelveHourFormat: false,
 
   // Greetings
-  greetingMorning: 'Good morning!',
-  greetingAfternoon: 'Good afternoon,',
-  greetingEvening: 'Good evening,',
-  greetingNight: 'Go to Sleep!',
+  greetingMorning: "Good morning!",
+  greetingAfternoon: "Good afternoon,",
+  greetingEvening: "Good evening,",
+  greetingNight: "Go to Sleep!",
 
   // Weather
-  weatherKey: 'InsertYourAPIKeyHere123456',
-  weatherIcons: 'OneDark', // 'Nord', 'Dark', 'White'
-  weatherUnit: 'C', // 'F', 'C'
-  language: 'en', // More languages in https://openweathermap.org/current#multi
+  weatherKey: "6375b1340fa5049797e7cbb6e61ac915",
+  weatherIcons: "OneDark", // 'Nord', 'Dark', 'White'
+  weatherUnit: "C", // 'F', 'C'
+  language: "en", // More languages in https://openweathermap.org/current#multi
 
   trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-  defaultLatitude: '37.775',
-  defaultLongitude: '-122.419',
+  defaultLatitude: "13.082680",
+  defaultLongitude: "80.270721",
 
   // ┌─┐┌─┐┬─┐┌┬┐┌─┐
   // │  ├─┤├┬┘ ││└─┐
@@ -39,41 +39,41 @@ const CONFIG = {
   // Links
   cards: [
     {
-      id: '1',
-      name: 'Github',
-      icon: 'github',
-      link: 'https://github.com/',
+      id: "1",
+      name: "Github",
+      icon: "github",
+      link: "https://github.com/",
     },
+    // {
+    //   id: "2",
+    //   name: "Mail",
+    //   icon: "mail",
+    //   link: "https://mail.protonmail.com/",
+    // },
+    // {
+    //   id: "3",
+    //   name: "Todoist",
+    //   icon: "trello",
+    //   link: "https://calendar.google.com/calendar/r",
+    // },
+    // {
+    //   id: "4",
+    //   name: "Calendar",
+    //   icon: "calendar",
+    //   link: "https://calendar.google.com/calendar/r",
+    // },
     {
-      id: '2',
-      name: 'Mail',
-      icon: 'mail',
-      link: 'https://mail.protonmail.com/',
+      id: "5",
+      name: "Reddit",
+      icon: "bookmark",
+      link: "https://reddit.com",
     },
-    {
-      id: '3',
-      name: 'Todoist',
-      icon: 'trello',
-      link: 'https://calendar.google.com/calendar/r',
-    },
-    {
-      id: '4',
-      name: 'Calendar',
-      icon: 'calendar',
-      link: 'https://calendar.google.com/calendar/r',
-    },
-    {
-      id: '5',
-      name: 'Reddit',
-      icon: 'bookmark',
-      link: 'https://reddit.com',
-    },
-    {
-      id: '6',
-      name: 'Odysee',
-      icon: 'youtube',
-      link: 'https://odysee.com/',
-    },
+    // {
+    //   id: "6",
+    //   name: "Odysee",
+    //   icon: "youtube",
+    //   link: "https://odysee.com/",
+    // },
   ],
 
   // ┬  ┬┌─┐┌┬┐┌─┐
@@ -81,45 +81,45 @@ const CONFIG = {
   // ┴─┘┴└─┘ ┴ └─┘
 
   //Icons
-  firstListIcon: 'music',
-  secondListIcon: 'coffee',
+  firstListIcon: "music",
+  secondListIcon: "coffee",
 
   // Links
   lists: {
     firstList: [
       {
-        name: 'Inspirational',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: "Inspirational",
+        link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
       },
       {
-        name: 'Classic',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: "Classic",
+        link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
       },
       {
-        name: 'Oldies',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: "Oldies",
+        link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
       },
       {
-        name: 'Rock',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: "Rock",
+        link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
       },
     ],
     secondList: [
       {
-        name: 'Linkedin',
-        link: 'https://linkedin.com/',
+        name: "Linkedin",
+        link: "https://linkedin.com/",
       },
       {
-        name: 'Figma',
-        link: 'https://figma.com/',
+        name: "Youtube",
+        link: "https://www.youtube.com/",
       },
       {
-        name: 'Dribbble',
-        link: 'https://dribbble.com',
+        name: "Google Meet",
+        link: "https://meet.google.com/",
       },
       {
-        name: 'Telegram',
-        link: 'https://webk.telegram.org',
+        name: "Github",
+        link: "https://github.com/",
       },
     ],
   },
